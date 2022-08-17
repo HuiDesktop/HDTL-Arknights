9618dede5aec3abe788b69f81e7a8a3da3c8d383
Pure01Fx <pure01fx@outlook.com>
2022-08-17 16:49:40 +0800

    feat: remove deprecated event.on

--- main.lua
+++ main.lua
@@ -107,8 +107,7 @@ ipc.addPanelItem(
 
 -- 绑定事件
 ev.on(window.draw, model.draw)
-ev.on(window.after_draw, idle_audios_eve);
-ev.on(window.after_draw, audio.update);
+ev.on(window.after_draw, idle_audios_eve)
 
 -- 处理idle
 enterStateThen["idle"] = function()
